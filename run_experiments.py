74e531348fff04e2fd940da4b8bcae75a1419f15
arvindrajaraman <arvind6902@gmail.com>
2023-12-11 22:16:10 -0800

    changes

--- run_experiments.py
+++ run_experiments.py
@@ -10,4 +10,4 @@ for regularizer in ["none", "weight_mag", "regenerative_reg", "singular_loss"]:
     for lambda_ in [0.01, 0.1, 1.0]:
         for layer_discount in [0.5, 0.75, 1.0, 1.25, 1.5]:
             for update_with_target in [False, True]:
-                subprocess.run(f"CUDA_VISIBLE_DEVICES={args.seed} python cs285/scripts/run_hw3_dqn.py -cfg experiments/dqn/lunarlander.yaml --seed {args.seed} --regularizer \"{regularizer}\" --weight_plot_freq 1000 --lambda_ {lambda_} --layer_discount {layer_discount} --update_with_target {update_with_target}", shell=True)
+                subprocess.run(f"CUDA_VISIBLE_DEVICES={args.seed} python cs285/scripts/run_hw3_dqn.py -cfg experiments/dqn/lunarlander.yaml --seed {args.seed} --regularizer \"{regularizer}\" --weight_plot_freq 1000 --lambda_ {lambda_} --layer_discount {layer_discount} --update_with_target {update_with_target} --which_gpu {args.seed}", shell=True)
